413b0c5a0ff1a1573e1632c7a48924a0ff0d5f4e
David Nekovář <nekovard@gmail.com>
2018-01-16 15:06:46 +0100

    notes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use('/api/v0', routes);
 
 app.listen(process.env.port || 4000, function(){
-    console.log('now listening for requests');
+    console.log('now listening for requests on localhost:4000');
 });
 
 app.get('/', (req, res) => {
