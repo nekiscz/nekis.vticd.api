b68a1eb24c2d4cd125aa39a9f0bc4f82a9771cfd
David Nekovář <nekovard@gmail.com>
2018-01-18 17:45:58 +0100

    changed label transformation
    from objects in array in array, to objects in array...

--- data/transform.js
+++ data/transform.js
@@ -1,20 +1,20 @@
 function category(obj) {
     // console.log(`${obj.kind} - ${obj.code}`)
 
-    if (obj.kind != 'category') {
-        console.log(`--- object doesn't contain category tag`);
-        return;
-    }
+    // if (obj.kind != 'category') {
+    //     console.log(`--- object doesn't contain category tag`);
+    //     return;
+    // }
 
     transformed = {
-        type: "category",
+        type: obj.kind,
         code: obj.code,
         languages: [
             "eng",
             "vn"
         ],
         department: "",
-        meta: obj.meta,
+        meta: obj.meta ? obj.meta : '',
         parent: obj.superclass ? obj.superclass.code : '',
         children: obj.subclass ? (obj.subclass.length ? obj.subclass.map((object) => {return object.code}) : [obj.subclass.code]) : [],
         labels : {
@@ -30,25 +30,25 @@ function transformLabel(l) {
     var transformed = [];
 
     if (l.label.fragment) {
-        transformed = [ ...transformed, { 
+        transformed = { 
             kind: l.kind, 
             label: l.label.fragment.length ? l.label.fragment.map((obj) => {
                 return { text: obj._, type: obj.type } 
             }): { text: l.label.fragment._, type: l.label.fragment.type }
-        }];
+        };
     }
 
     if (l.label._) {
-        transformed = [...transformed, {
+        transformed =  {
             kind: l.kind,
             label: [{
                 text: l.label._,
                 type: "text"
             }]
-        }];
+        };
     }
 
-    console.log(`transformed labels: ${JSON.stringify(transformed)}`)
+    //console.log(`transformed labels: ${JSON.stringify(transformed)}`)
     return transformed;
 }
 
